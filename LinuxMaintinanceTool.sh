0059e9688cc1d959572ae478e4008b7d951c3562
Sammeeeeeeee <s.patcas18@gmail.com>
2024-07-20 21:07:23 +0100

    Remove scroll option ad add download option to deployment tool

--- LinuxMaintinanceTool.sh
+++ LinuxMaintinanceTool.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 full_update() {
-    sudo apt update
-    sudo apt full-upgrade -y
-    sudo apt autoremove --purge -y
-    sudo snap refresh
+    timeout 300 sudo apt update
+    timeout 300 sudo apt full-upgrade -y
+    timeout 300sudo apt autoremove --purge -y
+    timeout 300sudo snap refresh
 }
 
 docker_update() {
-    docker run --name wathtower_docker-updater --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower:latest --run-once --cleanup --stop-timeout 60s --include-restarting --include-stopped
+    timeout 3000 docker run --name wathtower_docker-updater --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower:latest --run-once --cleanup --stop-timeout 60s --include-restarting --include-stopped
 }
 
 disk_usage() {
-    df -h
+    timeout 30 df -h
 }
 
 docker_status() {
-    docker ps -a
+    timeout 30 docker ps -a
 }
 
 memory_free_average() {
-    free -m | awk 'NR==2{printf "Average Memory Usage: %.2f%%\n", $3*100/$2 }'
+    timeout 30 free -m | awk 'NR==2{printf "Average Memory Usage: %.2f%%\n", $3*100/$2 }'
 }
 
 top_processes_average() {
-    ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6
+    timeout 30 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6
 }
 
 UPDATE_CHOICES=$(whiptail --title "Select Update Options" --separate-output --checklist \
@@ -50,11 +50,11 @@ for choice in $UPDATE_CHOICES; do
     case $choice in
         "1")
             output=$(full_update 2>&1)
-            whiptail --title "Full System Update" --scrolltext --msgbox "$output" 20 80
+            whiptail --title "Full System Update"  --msgbox "$output" 20 80
             ;;
         "2")
             output=$(docker_update 2>&1)
-            whiptail --title "Docker Update (Watchtower)" --scrolltext --msgbox "$output" 20 80
+            whiptail --title "Docker Update (Watchtower)" --msgbox "$output" 20 80
             ;;
     esac
 done
@@ -63,19 +63,19 @@ for choice in $INFO_CHOICES; do
     case $choice in
         "1")
             output=$(disk_usage 2>&1)
-            whiptail --title "Disk Usage Check" --scrolltext --msgbox "$output" 20 80
+            whiptail --title "Disk Usage Check" --msgbox "$output" 20 80
             ;;
         "2")
             output=$(docker_status 2>&1)
-            whiptail --title "Docker Container Status" --scrolltext --msgbox "$output" 20 80
+            whiptail --title "Docker Container Status" --msgbox "$output" 20 80
             ;;
         "3")
             output=$(memory_free_average 2>&1)
-            whiptail --title "Memory Free Average" --scrolltext --msgbox "$output" 20 80
+            whiptail --title "Memory Free Average" --msgbox "$output" 20 80
             ;;
         "4")
             output=$(top_processes_average 2>&1)
-            whiptail --title "Top Processes Average" --scrolltext --msgbox "$output" 20 80
+            whiptail --title "Top Processes Average" --msgbox "$output" 20 80
             ;;
     esac
 done
